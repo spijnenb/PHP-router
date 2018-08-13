b762b639c700b29f7dd324abb0db2c561309961b
DeSteef <stefanpijnenburg@outlook.com>
2018-08-13 16:07:54 +0200

    add readme.md and default default routes

--- index.php
+++ index.php
@@ -4,18 +4,12 @@ require("./assets/php/classes/router.php");
 
 $router = new Router();
 
-$router->get("/demo", function($req, $res){
-	$res->render("demo.html");
+$router->get("/", function($req, $res){
+	$res->send("Index Route");
 });
 
-$router->post("/apples/:tree/:brand", function($req, $res){
-	$params = $req->getParams();
-	$body = $req->getBody();
-	$res->send(
-		"You have reached the apples POST route. Your tree is "
-		. $params[0] . " and your brand is " . $params[1] . "\n" .
-		"You have submitted the following data: " . $body
-	);
+$router->get("/demo", function($req, $res){
+	$res->render("demo.html");
 });
 
 $router->set404();
